030abb084f07f1c34e0f7e681ca4c62e5aa62aff
LuvMePlz <demon-u3ada@yandex.ru>
2019-03-22 21:27:47 +0300

    true packeges

---
+++ Linear_Reg/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MathNet.Numerics.LinearAlgebra;
+
+namespace Linear_Reg
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var A = Matrix<double>.Build.DenseDiagonal(5, 1);
+            Console.WriteLine(A);
+        }
+    }
+}
